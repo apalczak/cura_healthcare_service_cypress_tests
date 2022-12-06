3ca4dfa3bf7bf9cd80b4e86aeb60141b36ab3776
ArkPal <arkadiusz.palczak@gmail.com>
2022-12-06 22:59:14 +0100

    Sign in tests

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,47 @@
+describe("Sign in to the CURA Healthcare page", () => {
+    beforeEach(() => {
+        cy.visit("/");
+    });
+
+    it("Signs in with proper username and password and then signs out", () => {
+        cy.selectToggledMenuOption("Login");
+        cy.typeProperLoginData("#demo_username_label", "#txt-username");
+        cy.typeProperLoginData("#demo_password_label", "#txt-password");
+        cy.get("#btn-login").scrollIntoView().click({ force: true });
+        cy.get("#menu-toggle").scrollIntoView().click();
+        cy.get("#sidebar-wrapper").find("li").should("contain", "Logout");
+        cy.selectToggledMenuOption("Logout");
+        cy.get("#sidebar-wrapper").find("li").should("contain", "Login");
+    });
+
+    it("Signs in with improper password", () => {
+        cy.selectToggledMenuOption("Login");
+        cy.typeProperLoginData("#demo_username_label", "#txt-username");
+        cy.get("#txt-password").type("ThisIsImproperPassword");
+        cy.get("#btn-login").scrollIntoView().click({ force: true });
+        cy.get("#login").should(
+            "contain",
+            "Login failed! Please ensure the username and password are valid."
+        );
+    });
+
+    it("Signs in without any username", () => {
+        cy.selectToggledMenuOption("Login");
+        cy.typeProperLoginData("#demo_password_label", "#txt-password");
+        cy.get("#btn-login").scrollIntoView().click({ force: true });
+        cy.get("#login").should(
+            "contain",
+            "Login failed! Please ensure the username and password are valid."
+        );
+    });
+
+    it("Signs in without any password", () => {
+        cy.selectToggledMenuOption("Login");
+        cy.typeProperLoginData("#demo_username_label", "#txt-username");
+        cy.get("#btn-login").scrollIntoView().click({ force: true });
+        cy.get("#login").should(
+            "contain",
+            "Login failed! Please ensure the username and password are valid."
+        );
+    });
+});
